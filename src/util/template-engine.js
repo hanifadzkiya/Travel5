0b31485e54aa2e1caaddae3c40c73f975606ad24
hanifadzkiya <hanif@oyindonesia.com>
2020-12-12 20:03:03 +0700

    Delete unused file:

--- src/util/template-engine.js
+++
@@ -1,9 +0,0 @@
-const ejs = require("ejs");
-const fs = require("fs");
-
-const renderFromFile = (path, data) => {
-    let template = fs.readFileSync(path, encoding="utf-8");
-    return ejs.render(template, data);
-}
-
-module.exports = { renderFromFile };

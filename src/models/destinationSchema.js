2680a47cdf5534d05f0e4a35ab6d98ec727a6206
hanifadzkiya <hanif@oyindonesia.com>
2020-12-17 18:49:27 +0700

    Format document with pretifier

--- src/models/destinationSchema.js
+++ src/models/destinationSchema.js
@@ -4,49 +4,55 @@ const Schema = mongoose.Schema;
 require("mongoose-currency").loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
-var reviewSchema = new Schema({
-  userId: {
-    type: mongoose.Types.ObjectId,
-    ref: "user"
-  },
-  rating: {
+var reviewSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+    },
+    rating: {
       type: Number,
       min: 1,
       max: 5,
-      required: true
-  },
-  review: {
-      type: String,
-      required: true
-  }
-}, {
- timestamps: true 
-});
-
-var destinationSchema = new Schema({
-  name: {
+      required: true,
+    },
+    review: {
       type: String,
       required: true,
+    },
   },
-  address: {
+  {
+    timestamps: true,
+  }
+);
+
+var destinationSchema = new Schema(
+  {
+    name: {
       type: String,
-      required: true
-  },
-  lat: Number,
-  long: Number,
-  images: [String],
-  price: {
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    lat: Number,
+    long: Number,
+    images: [String],
+    price: {
       type: Currency,
       required: true,
-      min: 0
-  },
-  category: {
+      min: 0,
+    },
+    category: {
       type: String,
-      required: true
+      required: true,
+    },
+    reviews: [reviewSchema],
   },
-  reviews: [reviewSchema]
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Destination", destinationSchema);
--- src/models/paketWisata.js
+++ src/models/paketWisata.js
@@ -3,72 +3,70 @@ const Schema = mongoose.Schema;
 
 require("mongoose-currency").loadType(mongoose);
 
-const hotelSchema = new Schema(
-    {
-        hotelId: {
-            type: mongoose.Types.ObjectId,
-            require: true
-        },
-        roomId: {
-            type: mongoose.Types.ObjectId,
-            require: true
-        },
-        start_date: Date,
-        end_date: Date
-    }
-);
+const hotelSchema = new Schema({
+  hotelId: {
+    type: mongoose.Types.ObjectId,
+    require: true,
+  },
+  roomId: {
+    type: mongoose.Types.ObjectId,
+    require: true,
+  },
+  start_date: Date,
+  end_date: Date,
+});
 
-const tempatwisataSchema = new Schema(
-    {
-        tempat_wisataId: {
-            type: mongoose.Types.ObjectId,
-            require: true
-        },
-        start_date: Date,
-        end_date: Date
-    }
-);
+const tempatwisataSchema = new Schema({
+  tempat_wisataId: {
+    type: mongoose.Types.ObjectId,
+    require: true,
+  },
+  start_date: Date,
+  end_date: Date,
+});
 
-const reviewSchema = new Schema(
-    {
-        userId: {
-            type: mongoose.Types.ObjectId,
-            required: true
-        },
-        rating: Number,
-        review: String
-    }
-);
+const reviewSchema = new Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  rating: Number,
+  review: String,
+});
 
 const paketwisataSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        start_date: {
-            type: Date,
-            required: true
-        },
-        end_date: {
-            type: Date,
-            required: true
-        },
-        total_price: mongoose.Types.Currency,
-        hotels: {
-            type: [hotelSchema],
-            required: true
-        },
-        tempat_wisata: {
-            type: [tempatwisataSchema],
-            required: true
-        },
-        reviews: [reviewSchema]
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    start_date: {
+      type: Date,
+      required: true,
+    },
+    end_date: {
+      type: Date,
+      required: true,
+    },
+    total_price: mongoose.Types.Currency,
+    hotels: {
+      type: [hotelSchema],
+      required: true,
+    },
+    tempat_wisata: {
+      type: [tempatwisataSchema],
+      required: true,
+    },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-var PaketWisata =  mongoose.model("paketwisata", paketwisataSchema, "paketwisata")
+var PaketWisata = mongoose.model(
+  "paketwisata",
+  paketwisataSchema,
+  "paketwisata"
+);
 module.exports = PaketWisata;

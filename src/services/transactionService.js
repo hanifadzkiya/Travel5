3b88d185b087d97a913fd08f8234a71455fd411e
hanifadzkiya <hanif@oyindonesia.com>
2020-12-18 21:38:14 +0700

    Finishing pgateway

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -3,6 +3,7 @@ const Users = require("../models/user");
 const emailUtil = require("../util/email-util");
 const qrcodeUtil = require("../util/qrcode-utils");
 const commonUtil = require("../util/commonUtil");
+const paymentService = require("../services/payment-service");
 const insertTransaction = async (username, jenisTransaksi, detailTransaksi) => {
   const result = await Users.findOneAndUpdate(
     { username: username },
@@ -23,7 +24,17 @@ const insertTransaction = async (username, jenisTransaksi, detailTransaksi) => {
     "http://127.0.0.1:3000/tour/detail/hanifadzkiya/5fdc9f443b1fefbc2d3aa0c7"
   );
   result.qrcode = qrcode;
-  // emailUtil.sendEmail(result.email, "Your Package is Ready", `<img src="${qrcode}" width="500" height="600">`);
+  console.log("HSHSHS");
+  const urlPayment = await paymentService.createPaymentUrl(
+    { amount: 10000 },
+    {
+      first_name: result.user,
+      last_name: result.username,
+      email: result.email,
+      phone: result.phone,
+    }
+  );
+  emailUtil.sendEmail(result.email, "Your Package is Ready", `Please pay your bill in <a href="${urlPayment}"> here </a>`);
   // console.log(`<img src="${qrcode}" width="500" height="600">`);
   return result;
 };

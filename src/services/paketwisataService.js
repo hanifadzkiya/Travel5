b70d57e4b37c7614cb65a9b06b71d22cc4a34ea9
Hilman Abdan <mystald@github.com>
2020-12-18 13:24:20 +0700

    CRUD Reviews for Paket Wisata

--- src/services/paketwisataService.js
+++ src/services/paketwisataService.js
@@ -25,6 +25,48 @@ const deleteById = async (id) => {
   return await PaketWisata.findByIdAndDelete(id);
 };
 
+const getAllReview = async (id) => {
+  paketWisata = await PaketWisata.findById(id); 
+  return paketWisata.reviews;
+};
+
+const addReview = async (id, newReview) => {
+  return await PaketWisata.findByIdAndUpdate(
+    id, 
+    { $push: { "reviews": {userId: newReview.userId, rating: newReview.rating, review: newReview.review} } },
+    { new: true }
+  );
+};
+
+const deleteAllReview = async (id) => {
+  return await PaketWisata.findByIdAndUpdate(
+    id, 
+    { $set: { "reviews": [] } },
+    { new: true }
+  );
+};
+
+const getReviewById = async (idPaket, idReview) => {
+  paketWisata = await PaketWisata.findById(idPaket);
+  return paketWisata.reviews.id(idReview);
+};
+
+const updateReviewById = async (idPaket, idReview, newReview) => {
+  paketWisata = await PaketWisata.findById(idPaket);
+  oldReview = paketWisata.reviews.id(idReview);
+  oldReview.rating = newReview.rating;
+  oldReview.review = newReview.review;
+  return paketWisata.save()
+};
+
+const deleteReviewById = async (idPaket, idReview) => {
+  return await PaketWisata.findByIdAndUpdate(
+    idPaket,
+    { $pull: { "reviews": { _id: idReview } } },
+    { new: true }
+  );
+};
+
 module.exports = {
   getAll,
   add,
@@ -33,4 +75,12 @@ module.exports = {
   getById,
   updateById,
   deleteById,
+
+  getAllReview,
+  addReview,
+  deleteAllReview,
+
+  getReviewById,
+  updateReviewById,
+  deleteReviewById
 };

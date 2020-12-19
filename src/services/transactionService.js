213e5f0af5dab812be5f97c3e9c98a21f1514c3a
hanifadzkiya <hanif@oyindonesia.com>
2020-12-19 12:54:09 +0700

    format doc

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -22,7 +22,7 @@ const insertTransaction = async (username, jenisTransaksi, detailTransaksi) => {
   }/detail/${username}/${transactionId}`;
   const qrcode = await qrcodeUtil.generate(url);
   console.log(qrcode);
-  Object.assign(result, {"qrcode": qrcode});
+  Object.assign(result, { qrcode: qrcode });
   const urlPayment = await paymentService.createPaymentUrl(
     { amount: 10000 },
     {
@@ -32,9 +32,17 @@ const insertTransaction = async (username, jenisTransaksi, detailTransaksi) => {
       phone: result.phone,
     }
   );
-  require("fs").writeFileSync(`public/images/${transactionId}.png`, qrcode.replace(/^data:image\/png;base64,/, ""), 'base64');
+  require("fs").writeFileSync(
+    `public/images/${transactionId}.png`,
+    qrcode.replace(/^data:image\/png;base64,/, ""),
+    "base64"
+  );
   const urlImage = `http://${commonUtil.getPublicIp()}/images/${transactionId}.png`;
-  emailUtil.sendEmail(result.email, "Your Package is Ready", `Please pay your bill in <a href="${urlPayment}"> here </a> Qr code : <img src="${urlImage}" height="400" width="400"/>`);
+  emailUtil.sendEmail(
+    result.email,
+    "Your Package is Ready",
+    `Please pay your bill in <a href="${urlPayment}"> here </a> Qr code : <img src="${urlImage}" height="400" width="400"/>`
+  );
   return result;
 };
 
